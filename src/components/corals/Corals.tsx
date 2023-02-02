ee1f26dc5bafec4d252000ebe2c71473fffa5aeb
elis-11 <eliza.arzanukaeva@gmail.com>
2023-02-02 18:52:43 +0100

    Displayd Corals

--- src/components/corals/Corals.tsx
+++ src/components/corals/Corals.tsx
@@ -6,14 +6,6 @@ import { Coral } from "../../types/corals";
 
 export const Corals = () => {
   const [corals, setCorals] = useState(coralsJson as [Coral]);
-  const [newCoral, setNewCoral] = useState<Coral>({
-    _id: Date.now().toString(),
-    name: "",
-    size: 30,
-    // url: "https://source.unsplash.com/200x200/?coral",
-    url: "https://i.pravatar.cc",
-    color : ""
-  })
   console.log(corals);
 
   return (
@@ -23,7 +15,7 @@ export const Corals = () => {
         {corals.map((coral) => (
           <div key={coral._id} className="coral">
             <img src={coral.url} alt="" />
-            <div className="name">{coral._id}</div>
+            {/* <div className="name">{coral._id}</div> */}
             <div className="name">{coral.name}</div>
             <div className="name">{coral.color}</div>
             <div className="name">{coral.size} &nbsp; sm</div>
